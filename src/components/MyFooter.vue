0455a0cc91d547797c69dad56544e0971dfef086
JEH <han880330@yahoo.co.jp>
2021-10-21 18:20:33 +0900

    TodoListのhtml,cssコードを作成しVueに適応、チェックボックスを編集

---
+++ src/components/MyFooter.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="todo-footer">
+      <label>
+          <input type="checkbox"/>
+      </label>
+      <span>
+          <span>Finish0</span>/ All2
+      </span>
+      <button class="btn btn-danger">Clear finish list</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'MyFooter'
+}
+</script>
+
+<style scoped>
+.todo-footer{
+    height: 40px;
+    line-height: 40px;
+    padding-left: 6px;
+    margin-top: 5px;
+}
+
+.todo-footer label{
+    display: inline-block;
+    margin-right: 20px;
+    cursor: pointer;
+}
+
+.todo-footer label input{
+    position: relative;
+    top: -1px;
+    vertical-align: middle;
+    margin-right: 5px;
+}
+
+.todo-footer button{
+    float: right;
+    margin-top: 5px;
+}
+</style>
---
+++ src/components/MyHeader.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="todo-header">
+      <input type="text" placeholder="Add your todo list,push Enter key to compulite">
+  </div>
+</template>
+
+<script>
+export default {
+    name:'MyHeader'
+}
+</script>
+
+<style scoped>
+.todo-header input{
+    width: 97%;
+    height: 28px;
+    font-size: 15px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    padding: 4px 7px;
+}
+
+.todo-header input:focus {
+    outline: none;
+    border-color: rgba(82, 168, 236, 0.8);
+    box-shadow: inset 0 1px 1px rgba(0,0,0,0.075),0 0 8px rgba(82, 168, 236, 0.6)
+}
+</style>
